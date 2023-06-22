5a2e7334e7512c64ec1f4d6d7fa88ff309ea9e41
Jan Hovland <jan.hovland@lyse.net>
2023-06-22 20:48:11 +0200

    Initial Commit

---
+++ Janemann/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Janemann
+//
+//  Created by Jan Hovland on 22/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Janemann/JanemannApp.swift
@@ -0,0 +1,17 @@
+//
+//  JanemannApp.swift
+//  Janemann
+//
+//  Created by Jan Hovland on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct JanemannApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
